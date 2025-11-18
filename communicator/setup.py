3ef3382d6e8c7a0f4825f97c497be82099fb3811
umohkenn <umohkenn@gmail.com>
2025-11-18 17:12:56 +0100

    Add Communicator packaging configuration

--- communicator/setup.py
+++ communicator/setup.py
@@ -1,18 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("README.md") as f:
-    long_description = f.read()
-
-setup(
-    name="communicator",
-    version="0.0.1",
-    description="Education-focused messaging module for Frappe",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="DappoF",
-    author_email="support@example.com",
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=["frappe>=14.0.0"],
-)
+if __name__ == "__main__":
+    setup()
