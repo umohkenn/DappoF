3974f059071534202965ab4f217f0e151d4086d2
umohkenn <umohkenn@gmail.com>
2026-02-13 20:45:37 +0100

    fix(frappe): add valid pyproject.toml at app directory root

---
+++ education/education/config/desktop.py
@@ -0,0 +1,11 @@
+from frappe import _
+
+
+def get_data():
+	return [
+		{
+			"module_name": "Education",
+			"type": "module",
+			"label": _("Education"),
+		}
+	]
---
+++ education/education/hooks.py
@@ -0,0 +1,11 @@
+app_name = "education"
+app_title = "Education"
+app_publisher = "DappoF"
+app_description = "Custom app for curriculum transition planning"
+app_email = "devnull@example.com"
+app_license = "mit"
+
+# Include doctype JS assets
+# doctype_js = {
+# 	"Curriculum Transition Plan": "doctype/curriculum_transition_plan/curriculum_transition_plan.js"
+# }
